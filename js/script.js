20897aa6f7632e0f5d99ea50dbdeda1cee577dcc
MaxPopsuy <maim09p@gmail.com>
2021-06-23 22:08:08 +0300

    endGame logic added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 import * as functions from './functions.js';
 
-{/* <li class="table__cell"></li> */}
+{/* <li class="table__cell"></li> */ }
+// const table = document.querySelectorAll('.table__cell');
+
+// table.forEach((el) => {
+//   el.addEventListener('click', functions.checkForEnd(table));
+// });
