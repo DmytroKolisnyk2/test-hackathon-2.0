c752b7111fa99699ea610c795ae7a935bcb6469d
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2021-06-23 23:50:43 +0300

    add end game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-import * as functions from './functions.js';
-
+import * as functions from "./functions.js";
+localStorage.setItem('test', 1);
 functions.drawTable();
-
+document.querySelector(".content__reset").addEventListener("click", () => {
+   functions.drawTable();
+   localStorage.clear();
+});
